a9732950169168c03d83f33234e2eec4335f4bb2
Nam Lê <lenam07152002@gmail.com>
2023-10-10 19:01:07 +0700

    base*

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGODB_LOCAL;
+
+app.use(express.json());
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+mongoose
+   .connect(MONGO_URL)
+   .then(() => console.log('connected to db'))
+   .catch((err) => console.log(`error in connect db : ${err}`));
+app.listen(PORT, () => {
+   console.log(`listening success ${PORT}`);
+});
