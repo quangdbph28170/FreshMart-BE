44c3de51b693b9e516023531cb865919f322b133
DevidMonster <mrbat905@gmail.com>
2023-10-25 10:06:44 +0700

    update product controller

--- src/routers/products.js
+++ src/routers/products.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post("/products", createProduct);
 router.patch("/products/:id", updateProduct);
 router.get("/products", getProducts);
-router.get("/products/related/:id", getRelatedProducts);
+router.get("/products/related/:cate_id/:product_id", getRelatedProducts);
 router.get("/products/:id", getOneProduct);
 router.delete("/products/:id", removeProduct);
 export default router;
