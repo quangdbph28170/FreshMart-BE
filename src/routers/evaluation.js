7dcc917fc5c5373e4211ede3cfcd38b44d4fa1ea
DevidMonster <mrbat905@gmail.com>
2023-11-26 23:55:07 +0700

    update ratingr

--- src/routers/evaluation.js
+++ src/routers/evaluation.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authentication from '../middleware/authentication';
-import { createEvaluation, getIsRatedByProductId, getIsRatedDetail, isReviewVisible } from '../controllers/evaluation';
+import { createEvaluation, getAllRating, getIsRatedByProductId, getIsRatedDetail, isReviewVisible } from '../controllers/evaluation';
 import { authorization } from '../middleware/authorization';
 
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/evaluation', authentication, createEvaluation);
 router.get('/evaluationByProductId/:id', getIsRatedByProductId);
 router.get('/evaluation/:id', getIsRatedDetail);
+router.get('/evaluation/', getAllRating);
 router.patch('/evaluation/:id', authorization,isReviewVisible);
 
 export default router;
